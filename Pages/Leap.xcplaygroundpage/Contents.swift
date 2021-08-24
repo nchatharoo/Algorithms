523f56f88e4b73960831656c4442272b091a36cc
Nadheer Chatharoo <>
2021-08-24 17:40:40 +0200

    Create a test list  to reflect the description

--- Pages/Leap.xcplaygroundpage/Contents.swift
+++ Pages/Leap.xcplaygroundpage/Contents.swift
@@ -12,4 +12,23 @@ For example, 2021 is not a leap year, but 2020 is. 1900 is not a leap year, but 
 // year divisible by 400 = leap
 // any other year is not leap
 
+class LeapYearTests : XCTestCase {
+    
+    func test_evenlyDivisible_by4_isLeap() {
+        
+    }
+    
+    func test_evenlyDivisible_by100_isNotLeap() {
+        
+    }
+    
+    func test_evenlyDivisible_by400_isLeap() {
+        
+    }
+    
+    func test_evenlyDivisible_by4Or400_isNotLeap() {
+        
+    }
+}
+
 //: [Next](@next)
