59bfdd67366f5abd8da1370ffd78b86960f13d45
Nadheer Chatharoo <>
2021-08-24 17:37:57 +0200

    Create Leap playground page

---
+++ Pages/Leap.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: [Previous](@previous)
+
+import Foundation
+import XCTest
+
+/* Write a function that, given an integer representing a year, tells whether the year is leap or not.
+A leap year in the Gregorian calendar occurs on every year that is evenly divisible by 4, except every year that is evenly divisible by 100, unless the year is also evenly divisible by 400.
+For example, 2021 is not a leap year, but 2020 is. 1900 is not a leap year, but 2000 is. */
+
+//: [Next](@next)
