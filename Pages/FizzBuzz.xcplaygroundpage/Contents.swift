e224423351ca35f2539a0038bc0a7ccfe98688ad
Nadheer Chatharoo <>
2021-08-25 17:23:05 +0200

    Create FizzBuzz page

---
+++ Pages/FizzBuzz.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import XCTest
+
+/* Write a program usin TDD that prints the number from 1 to 100.
+ For multiple of three print "Fizz"
+ For multiple of five print "Buzz"
+ For numbers which are multiples of both three and five, print "FizzBuzz".
+ */
+
+//: [Next](@next)
