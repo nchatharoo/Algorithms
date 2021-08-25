9b14df64ec84109cc2592d0d91fce77dd5bb63bd
Nadheer Chatharoo <>
2021-08-25 17:30:33 +0200

    Create 1st test to print default case

--- Pages/FizzBuzz.xcplaygroundpage/Contents.swift
+++ Pages/FizzBuzz.xcplaygroundpage/Contents.swift
@@ -1,11 +1,28 @@
-//: [Previous](@previous)
 
 import XCTest
 
-/* Write a program usin TDD that prints the number from 1 to 100.
- For multiple of three print "Fizz"
- For multiple of five print "Buzz"
+/* Write a program using TDD that prints the number from 1 to 100.
+ For multiple of three, print "Fizz"
+ For multiple of five, print "Buzz"
  For numbers which are multiples of both three and five, print "FizzBuzz".
  */
 
-//: [Next](@next)
+class FizzBuzzPrinter {
+    func print(_ number: Int) -> String {
+        return "1"
+    }
+}
+
+class FizzBuzzTests: XCTestCase {
+    
+    func test_print_defaultCase() {
+        
+        let sut = FizzBuzzPrinter()
+        
+        let result = sut.print(1)
+        
+        XCTAssertEqual(result, "1")
+    }
+}
+
+FizzBuzzTests.defaultTestSuite.run()
